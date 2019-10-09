a9337691cdb8f38c8f271f71b23886de6406ad84
FIAP <FIAP>
2019-10-08 22:33:28 -0300

    json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("oi")
+const express = require('express');
+
+const app = express();
+
+app.get('/users', (request, response, next) => {
+    console.log(request.query);
+    console.log(response);
+    response.json({retorno: "Heya"});
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${process.env.PORT}`)
+});
+
+console.log('MEU AMBIENTE', process.env.NODE_ENV);
