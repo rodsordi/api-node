bea6990c8e287e00c87a3fa0c5ad56f0a2f50e34
FIAP <FIAP>
2019-10-10 22:13:28 -0300

    rest

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDgsqmvMhR7EyRZCeR2Vc63z2lrY80Bpwk",
+    authDomain: "node-api-c30db.firebaseapp.com",
+    databaseURL: "https://node-api-c30db.firebaseio.com",
+    projectId: "node-api-c30db",
+    storageBucket: "node-api-c30db.appspot.com",
+    messagingSenderId: "914554005536",
+    appId: "1:914554005536:web:34dd81a561ec27184ff445"
+  };
--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 
+const firebase = require('firebase');
+const firebaseConfig = require('./config/firebase');
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
 const app = express();
 
+app.get('/users/:id', (request, response, next) => {
+    const id = request.params.id;
+
+    db.collection('users').doc(id).get()
+        .then(user => {
+            if (!user.exists) {
+                response.sendStatus(204);
+            }
+            response.json(user.data());
+        })
+        .catch(err => {
+            response.json({success: false, err});
+        });
+});
+
 app.get('/users', (request, response, next) => {
-    console.log(request.query);
-    console.log(response);
-    response.json({retorno: "Heya"});
+    db.collection('users').get()
+        .then(users => {
+            const result = [];
+            users.forEach(user => {
+                result.push(user.data());
+            });
+            if (!result.length) {
+                response.sendStatus(204);
+            }
+            response.json(result);
+        })
+        .catch(err => {
+            console.error("Error to retrieve users", err);
+            response.sendStatus(500);
+        });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening on port ${process.env.PORT}`)
 });
-
-console.log('MEU AMBIENTE', process.env.NODE_ENV);
