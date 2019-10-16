dc17119d8e5fd37f95923d5f374ea45ddd6724a1
FIAP <FIAP>
2019-10-15 22:41:19 -0300

    refactor

---
+++ config/default.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "UUmF73NUeAUjQisgzt6t7RCkssfNeGEvbgYoY/uK1qeEDTe+KxGkZEB3vYPzT5RtJhQX+uT9++qSQac4jzmmV0g=="
+}
--- index.js
+++ index.js
@@ -5,9 +5,48 @@ const firebaseConfig = require('./config/firebase');
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
+const bodyParser = require('body-parser');
+
+const verifyToken = require('./middlewares/verifyToken');
+
 const app = express();
 
-app.get('/users/:id', (request, response, next) => {
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${process.env.PORT}`)
+});
+
+const createToken = require('./utils/createToken');
+
+app.post('/auth', (request, response, next) => {
+    db.collection('users')
+        .where('email', '==', request.body.email)
+        .where('password', '==', request.body.password)
+        .get()
+        .then(qs => {
+            const u = qs.docs.map(user => user.data())[0];
+            if (u.exists) {
+                return response
+                    .status(401)
+                    .send({
+                        code: "not_found",
+                        message: "User not found"
+                    });
+            }
+
+            const id = u.id;
+            response.json({token: createToken({id}) });
+
+        })
+        .catch(err => {
+            console.error("Error to retrieve users", err);
+            response.sendStatus(500);
+        });
+});
+
+app.get('/users/:id', verifyToken, (request, response, next) => {
     const id = request.params.id;
 
     db.collection('users').doc(id).get()
@@ -35,9 +74,4 @@ app.get('/users', (request, response, next) => {
             console.error("Error to retrieve users", err);
             response.sendStatus(500);
         });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`App listening on port ${process.env.PORT}`)
 });
